04cb82c917b8965f871d2455f1134e814ea24f37
dharnihaa <153358527+dharnihaa@users.noreply.github.com>
2024-06-05 09:42:42 +0530

    Update exp4.py

--- python/exp4.py
+++ python/exp4.py
@@ -1,10 +1,26 @@
-num1=int(input("num1 "))
-num2=int(input("num2 "))
-num3=int(input("num3 "))
+no_of_elements = int(input('Enter the size of list: '))
+array =[]
+#Read Array elements
+print( 'Enter the elements of the list: ')
+for i in range(0,no_of_elements,1):
+ element=input()
+ array.append(int(element))
+#finding the first three numbers
+max1=0
+max2=0
+max3=0
+for i in range(0,no_of_elements,1):
+ if array[i] >max1:
+  max3 = max2
+  max2=max1
+  max1=array[i]
+ elif array[i] >max2:
+  max3=max2
+  max2=array[i]
+ elif array[i] >max3:
+  max3=array[i]
 
-if(num1>=num2 and num1>=num3):
-  print(num1)
-elif(num2>=num3 and num2>=num1):
-  print(num2)
-else:
-  print(num3)
+#Display the largest elements
+print( 'Max1:{}'.format(max1))
+print( 'Max2:{}'.format(max2))
+print( 'Max3:{}'.format(max3))
